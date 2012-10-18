47fa4f319b93b3283cd4f79dca9318a4ce792e2f
Tomer Diament <tomer.dmnt@gmail.com>
2012-10-18 12:45:22 +0200

    Removed console.log()s

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ var Carousel = require('carousel')
 module.exports = CarouselSlide;
 inherit(CarouselSlide, Carousel);
 
-  console.log('slider!!');
 function CarouselSlide(el, opts) {
   if (!(this instanceof CarouselSlide)) return new CarouselSlide(el, opts);
   Carousel.call(this, el, opts);
 
-  console.log('slider!!');
   this.forEach(function (item) {
     setTimeout(function () {
       console.log(item);
